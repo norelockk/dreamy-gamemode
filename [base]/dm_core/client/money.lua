607419609d48ab4b651ab81bda2487a9cdee8a42
Norbert Podbiegły <112294007+gtsos@users.noreply.github.com>
2023-05-26 19:37:56 +0200

    chore: fix spaces indenting in core

--- [base]/dm_core/client/money.lua
+++ [base]/dm_core/client/money.lua
@@ -1,11 +1,11 @@
 function getGamePlayerMoney()
-	return getPlayerMoney()
+  return getPlayerMoney()
 end
 
 function giveGamePlayerMoney(money, isPremium)
-	triggerServerEvent('core:onPlayerGiveMoney', resourceRoot, localPlayer, money, isPremium, getGamePlayerMoney())
+  triggerServerEvent('core:onPlayerGiveMoney', resourceRoot, localPlayer, money, isPremium, getGamePlayerMoney())
 end
 
 function takeGamePlayerMoney(money)
-	triggerServerEvent('core:onPlayerTakeMoney', resourceRoot, localPlayer, money, getGamePlayerMoney())
+  triggerServerEvent('core:onPlayerTakeMoney', resourceRoot, localPlayer, money, getGamePlayerMoney())
 end
--- [base]/dm_core/client/playtime.lua
+++ [base]/dm_core/client/playtime.lua
@@ -1,62 +1,68 @@
 local PAYDAY_DATA = {
-	normal = {
+  normal = {
     cash = 20,
     reputation = 1
   },
-	premium = {
+  premium = {
     cash = 80,
     reputation = 2
   }
 }
 
 local function payday()
-	local data = PAYDAY_DATA[getElementData(localPlayer, 'player:premium') and 'premium' or 'normal']
+  local data = PAYDAY_DATA[getElementData(localPlayer, 'player:premium') and 'premium' or 'normal']
   giveGamePlayerMoney(data.cash)
 
-	local reputation = getElementData(localPlayer, 'player:reputation')
-	reputation = reputation + data.reputation
-	setElementData(localPlayer, 'player:reputation', reputation)
-end 
+  local reputation = getElementData(localPlayer, 'player:reputation')
+  reputation = reputation + data.reputation
+  setElementData(localPlayer, 'player:reputation', reputation)
+end
 
-local session = 0 
+local session = 0
 local playtime = -1
 
 local function calculate()
-	-- if not getElementData(localPlayer, 'player:spawned') then return end
-	if getElementData(localPlayer, 'player:away') then return end
+  -- if not getElementData(localPlayer, 'player:spawned') then return end
+  if getElementData(localPlayer, 'player:away') then
+    return
+  end
 
-	if playtime == -1 then 
-		local p = getElementData(localPlayer, 'player:playtime') 
-		if not p then 
-			return 
-		else 
-			playtime = p
-		end
-	end
-	
-	playtime = playtime + 1 
-	session = session + 1
-	
-	if session > 3600 * 8 then -- 8 h
-		-- No-life
-	end
-	
-	if session > 0 and (session / 60) / 60 == math.floor((session / 60) / 60) and session % 60 == 0 then
-		payday()
-	end
-	
-	if getRealTime().hour >= 1 and getRealTime().hour <= 5 then 
-		-- Nocny gracz
-	end
-end 
+  if playtime == -1 then
+    local p = getElementData(localPlayer, 'player:playtime')
+    if not p then
+      return
+    else
+      playtime = p
+    end
+  end
+
+  playtime = playtime + 1
+  session = session + 1
+
+  if session > 3600 * 8 then -- 8 h
+    -- No-life
+  end
+
+  if session > 0 and (session / 60) / 60 == math.floor((session / 60) / 60) and session % 60 == 0 then
+    payday()
+  end
+
+  if getRealTime().hour >= 1 and getRealTime().hour <= 5 then
+    -- Nocny gracz
+  end
+end
 setTimer(calculate, 1000, 0)
 
 local function update()
-	-- if not getElementData(localPlayer, 'player:spawned') then return end
-	if getElementData(localPlayer, 'player:away') then return end
+  -- if not getElementData(localPlayer, 'player:spawned') then return end
+  if getElementData(localPlayer, 'player:away') then
+    return
+  end
 
-	if playtime == -1 then return end
+  if playtime == -1 then
+    return
+  end
 
-	triggerServerEvent('core:onPlayerUpdatePlaytime', resourceRoot, playtime, session)
+  triggerServerEvent('core:onPlayerUpdatePlaytime', resourceRoot, playtime, session)
 end
 setTimer(update, 60000 * 5, 0)
